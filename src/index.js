8c983f96cde133fbdbd966f96551742aebfab9cd
Patrick Walter <patrick-walter@debianPatrick.walter.com>
2020-01-26 14:17:46 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+"use strict"
+
+import React from "react";
+import App from "./app";
+import { AppContainer } from "react-hot-loader";
+import { render } from "react-dom";
+
+const renderApp = (NextApp) => {
+    render(
+        <AppContainer>
+            <NextApp/> 
+        </AppContainer>,  
+        document.querySelector('[data-js="app"]')
+    );
+}
+
+renderApp(App);
+
+if(module.hot){
+    module.hot.accept("./app", () => {
+        const NextApp = require("./app").default
+        renderApp(NextApp);
+    });
+}
+
+
+
+
+     
