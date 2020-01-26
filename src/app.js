88627347daa6a3980a33215341415b96c9349050
Patrick Walter <patrick-walter@debianPatrick.walter.com>
2020-01-26 14:54:26 -0300

    First draft

--- src/app.js
+++ src/app.js
@@ -6,8 +6,43 @@ class App extends Component{
     
     render(){
         return(
-            <div>
-                <h1>GitHub App</h1>
+            <div className="app">
+                <div className="search">
+                    <input type="search" placeholder="Digite o nome do usuario"/>
+                    <button>Buscar</button>
+                </div>
+
+                <div className="user-info">
+                    <img src="https://avatars2.githubusercontent.com/u/46614552?v=4" alt="Imagem de Perfil"/>
+                    <h1>
+                        <a href="https://github.com/PatrickWalter387">Patrick Walter</a>
+                    </h1>
+
+                    <ul className="repos-info">
+                        <li>Repositorios: 277</li>
+                        <li>Seguidores: 65</li>
+                        <li>Seguindo: 102</li>
+                    </ul>
+
+                    <div className="actions">
+                        <button>Ver Repositorios</button>
+                        <button>Ver Favoritos</button>
+                    </div>
+
+                    <div className="repos">
+                        <h2>Repositorios</h2>
+                        <ul>
+                            <li><a href="#">Nome do repositorio:</a></li>
+                        </ul>
+                    </div>
+
+                    <div className="starred">
+                        <h2>Favoritos</h2>
+                        <ul>
+                            <li><a href="#">Nome do repositorio:</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         );
     } 
