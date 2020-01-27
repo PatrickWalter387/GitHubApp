f4d4e129c779b6399cf4845565267ae89db9274a
Patrick Walter <patrick.2017316623@gmail.com>
2020-01-26 21:57:00 -0300

    stylization

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 import React, {Component} from "react";
 
+
 class App extends Component{
     
     render(){
@@ -9,29 +10,36 @@ class App extends Component{
             <div className="app">
                 <div className="search">
                     <input type="search" placeholder="Digite o nome do usuario"/>
-                    <button>Buscar</button>
                 </div>
 
                 <div className="user-info">
-                    <img src="https://avatars2.githubusercontent.com/u/46614552?v=4" alt="Imagem de Perfil"/>
-                    <h1>
-                        <a href="https://github.com/PatrickWalter387">Patrick Walter</a>
-                    </h1>
+                    <div className="photo">
+                        <img src="https://avatars2.githubusercontent.com/u/46614552?v=4" alt="Imagem de Perfil"/>
+                    </div>
 
-                    <ul className="repos-info">
-                        <li>Repositorios: 277</li>
-                        <li>Seguidores: 67</li>
-                        <li>Seguindo: 102</li>
-                    </ul>
+                    <div className="data">
+                        <h1 className="username">
+                            <a href="https://github.com/PatrickWalter387">Patrick Walter</a>
+                        </h1>
 
+                        <ul className="repos-info">
+                            <li>Repositorios: 277</li>
+                            <li>Seguidores: 67</li>
+                            <li>Seguindo: 102</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="lists">
                     <div className="actions">
-                        <button>Ver Repositorios</button>
-                        <button>Ver Favoritos</button>
+                        <button className="show-repositories">VER REPOSITÓRIOS</button>
+                        <button className="show-starred">VER FAVORITOS</button>
                     </div>
 
                     <div className="repos">
                         <h2>Repositorios</h2>
                         <ul>
+                            <li><a href="#">Nome do repositorio:</a></li>
                             <li><a href="#">Nome do repositorio:</a></li>
                         </ul>
                     </div>
@@ -39,6 +47,7 @@ class App extends Component{
                     <div className="starred">
                         <h2>Favoritos</h2>
                         <ul>
+                            <li><a href="#">Nome do repositorio:</a></li>
                             <li><a href="#">Nome do repositorio:</a></li>
                         </ul>
                     </div>
