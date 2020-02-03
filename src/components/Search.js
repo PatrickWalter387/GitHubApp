655a34f6feb87e3aa0c2e8c6b21f865ee54dd1d5
Patrick Walter <patrick.2017316623@gmail.com>
2020-02-02 21:45:35 -0300

    Api GitHub Request

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,23 @@
 import React from "react";
+import ajax from "@fdaciuk/ajax";
 
 const Search = () => (
     <div className="search">
-        <input type="search" placeholder="Digite o nome do usuario"/>
+        <input 
+            type="search" 
+            placeholder="Digite o nome do usuario"
+            onKeyUp={(e) => {
+                const ENTER = 13;
+                const keyCode = e.which || e.keyCode;
+
+                if(keyCode === ENTER){
+                    ajax().get(`https://api.github.com/users/${e.target.value}`)
+                      .then(result => {
+                          console.log(result)
+                      })
+                }
+            }}
+        />
     </div>
 );
 
